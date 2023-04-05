397551aa1e0bf2816e01e22b1445b29506d23864
GeorgeJuniorGG <g216741@dac.unicamp.br>
2023-04-05 09:57:57 -0300

    Adding initial helper functions

--- TCP_socket/src/operations.c
+++ TCP_socket/src/operations.c
@@ -1,7 +1,32 @@
 #include "operations.h"
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
+int register_profile(profile new_profile) {
+    FILE * file = fopen("../data/profiles.txt", "a");
+    fputs("ASDAS", file);
+    return 1;
+}
+
+char* get_all_profiles() {
+    return "";
+}
+
+char* get_all_profiles_from_major(char* major) {
+    return "";
+}
+
+char* get_all_profiles_from_ability(char* ability) {
+    return "";
+}
+
+char* get_all_profiles_from_graduation_year(int year) {
+    return "";
+}
+
+char* get_profile(char* email) {
+    return "";
+}
+
+char* remove_profile(char* email) {
+    return "";
 }
