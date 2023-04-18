f5ee3e40a1ccf6319bafee3b5d7483e6ca9ec1b9
GeorgeJuniorGG <g216741@dac.unicamp.br>
2023-04-17 22:10:40 -0300

    Improving comments

--- TCP_socket/src/client.c
+++ TCP_socket/src/client.c
@@ -11,51 +11,57 @@
 
 // Código baseado no Beej's guide, especialmente no capítulo 6
 
-#define PORT "8330" // the port client will be connecting to 
+#define PORT "8330" // the port client will be connecting to
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 100 // max number of bytes we can get at once
 
-// get sockaddr, IPv4 or IPv6:
+// Função responsável por retornar o endereço do socket adequado, seja IPv4 ou IPv6
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr); // IPv4
     }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr); // IPv6
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, numbytes;  
-    char buf[MAXDATASIZE];
-    struct addrinfo hints, *servinfo, *p;
+    int sockfd; // Socket que será criado
+    int numbytes;
+    char buf[MAXDATASIZE];                // Buffer para receber mensagem
+    struct addrinfo hints, *servinfo, *p; // Guardarão informações de endereço
     int rv;
-    char s[INET6_ADDRSTRLEN];
+    char s[INET6_ADDRSTRLEN]; // Buffer que erá utilizado apenas em caso de IPv6 da parte do servidor
 
-    if (argc != 2) {
-        fprintf(stderr,"usage: client hostname\n");
+    if (argc != 2) // TODO: Deve ser alterado para integrar com os projetos
+    {
+        fprintf(stderr, "usage: client hostname\n");
         exit(1);
     }
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+    memset(&hints, 0, sizeof hints); // Assegura que tudo será inicializado com zeros
+    hints.ai_family = AF_UNSPEC;     // Não especifica se será IPv4 ou IPv6
+    hints.ai_socktype = SOCK_STREAM; // Socket TCP
 
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) // Pega informações sobre o endereço do servidor
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next) // Faz um loop pelos resultados e conecta com o primeiro possível
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, // Erro ao criar socket do cliente
+                             p->ai_protocol)) == -1)
+        {
             perror("client: socket");
             continue;
         }
 
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) // Erro ao conectar
+        {
             close(sockfd);
             perror("client: connect");
             continue;
@@ -64,27 +70,30 @@ int main(int argc, char *argv[])
         break;
     }
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "client: failed to connect\n");
         return 2;
     }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            s, sizeof s);
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), // Obtém endereço do servidor, sendo IPv4 ou IPv6
+              s, sizeof s);
     printf("client: connecting to %s\n", s);
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo); // Libera pois as informações não serão mais necessárias
 
-    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+    // TODO: alterar para receber mais bytes
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) // Recebe os bytes enviados pelo servidor
+    {
         perror("recv");
         exit(1);
     }
 
-    buf[numbytes] = '\0';
+    buf[numbytes] = '\0'; // Adiciona caracter para marcar o final da string
 
-    printf("client: received '%s'\n",buf);
+    printf("client: received '%s'\n", buf);
 
-    close(sockfd);
+    close(sockfd); // Fecha o socket
 
     return 0;
 }
--- TCP_socket/src/server_socket.c
+++ TCP_socket/src/server_socket.c
@@ -11,70 +11,76 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
 // Código baseado no Beej's guide, especialmente no capítulo 6
 
-#define PORT "8330"             // Porta não utilizada por nenhum outro processo. Será responsável por receber conexões
-#define BACKLOG 10              // Número máximo de conexões pendentes na fila
+#define PORT "8330" // Porta não utilizada por nenhum outro processo. Será responsável por receber conexões
+#define BACKLOG 10  // Número máximo de conexões pendentes na fila
 
 // Função responsável por finalizar processos zumbis
 void sigchild_handler(int s)
 {
     int errno_backup = errno;
 
-    while(waitpid(-1, NULL, WNOHANG) > 0);
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
 
-    errno = errno_backup;       // O waitpid pode mudar o valor do errno, mas queremos preservá-lo
+    errno = errno_backup; // O waitpid pode mudar o valor do errno, mas queremos preservá-lo
 }
 
 // Função responsável por retornar o endereço do socket adequado, seja IPv4 ou IPv6
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);          // IPv4
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr); // IPv4
     }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);            // IPv6
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr); // IPv6
 }
 
-int main(void) {
-    int socket_fd;                          // Escuta conexões
-    int new_fd;                             // Novas conexões
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
+int main(void)
+{
+    int socket_fd; // Escuta conexões
+    int new_fd;    // Novas conexões
+    int yes = 1;
+    char s[INET6_ADDRSTRLEN]; // Buffer que erá utilizado apenas em caso de IPv6 da parte do cliente
 
-    struct addrinfo hints, *servinfo, *p;   // posteriormente vão guardar informações de endereço
-    struct sockaddr_storage their_addr;     // Guarda informações do endereço dos clientes
-    socklen_t sin_size;                     // Guarda informações sobre o tamanho do socket
-    struct sigaction sa;                    // Para tratar processos zumbis
+    struct addrinfo hints, *servinfo, *p; // posteriormente vão guardar informações de endereço
+    struct sockaddr_storage their_addr;   // Guarda informações do endereço dos clientes
+    socklen_t sin_size;                   // Guarda informações sobre o tamanho do socket
+    struct sigaction sa;                  // Para tratar processos zumbis
 
-    memset(&hints, 0, sizeof hints);        // Garantir que todos os bits estão zerados
-    hints.ai_family = AF_UNSPEC;            // Deixa para definir se é IPv4 ou IPv6 posteriormente
-    hints.ai_socktype = SOCK_STREAM;        // Define socket tipo TCP
-    hints.ai_flags = AI_PASSIVE;            // Pega o IP da própria máquina
+    memset(&hints, 0, sizeof hints); // Garantir que todos os bits estão zerados
+    hints.ai_family = AF_UNSPEC;     // Deixa para definir se é IPv4 ou IPv6 posteriormente
+    hints.ai_socktype = SOCK_STREAM; // Define socket tipo TCP
+    hints.ai_flags = AI_PASSIVE;     // Pega o IP da própria máquina
 
     int rv;
 
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) // Pega informações sobre o endereço do socket que recebe novas conexões
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((socket_fd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next) // Faz um loop pelos resultados e faz bind para o primeiro possível
+    {
+        if ((socket_fd = socket(p->ai_family, p->ai_socktype, // Erro ao criar socket do servidor
+                                p->ai_protocol)) == -1)
+        {
             perror("server: socket");
             continue;
         }
 
-        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
+        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, // Erro ao tentar permitir o reuso de endereços locais ao usar o bind
+                       sizeof(int)) == -1)
+        {
             perror("setsockopt");
             exit(1);
         }
 
-        if (bind(socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (bind(socket_fd, p->ai_addr, p->ai_addrlen) == -1) // Erro ao tentar fazer bind
+        {
             close(socket_fd);
             perror("server: bind");
             continue;
@@ -83,49 +89,56 @@ int main(void) {
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo); // Libera pois as informações não serão mais necessárias
 
-    if (p == NULL)  {
+    if (p == NULL)
+    {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
 
-    if (listen(socket_fd, BACKLOG) == -1) {
+    if (listen(socket_fd, BACKLOG) == -1)
+    {
         perror("listen");
         exit(1);
     }
 
-    sa.sa_handler = sigchild_handler; // finaliza processos zumbis
+    sa.sa_handler = sigchild_handler; // Finaliza processos zumbis
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
         perror("sigaction");
         exit(1);
     }
 
     printf("server: waiting for connections...\n");
 
-    while(1) {  // main accept() loop
+    while (1) // Loop em que o servidor espera por conexões e as aceita
+    {
         sin_size = sizeof their_addr;
         new_fd = accept(socket_fd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
+        if (new_fd == -1)
+        {
             perror("accept");
             continue;
         }
 
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
+        inet_ntop(their_addr.ss_family, // Obtém endereço do cliente, sendo IPv4 ou IPv6
+                  get_in_addr((struct sockaddr *)&their_addr),
+                  s, sizeof s);
         printf("server: got connection from %s\n", s);
 
-        if (!fork()) { // this is the child process
-            close(socket_fd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
+        if (!fork())
+        {                                                   // Cria processo filho para lidar com as conexões aceitas
+                                                            // Esse trecho só é executado pelos processos filhos
+            close(socket_fd);                               // O socket que aceita conexões deve ser fechado para os processos filhos
+            if (send(new_fd, "Hello, world!", 13, 0) == -1) // Envia mensagem ao cliente conectado a este processo filho
                 perror("send");
             close(new_fd);
             exit(0);
         }
-        close(new_fd);  // parent doesn't need this
+        close(new_fd); // Processo pai deve fechar o socket destinado aos processos filhos
     }
 
     return 0;
